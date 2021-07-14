cc0c5c2c02eb22d7114fd83477055b3efa7cb044
Milena Yamamoto <milenayamamoto@gmail.com>
2021-07-13 23:26:42 -0300

    First commit of Alurakut

---
+++ pages/index.js
@@ -0,0 +1,64 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+
+function ProfileSidebar(prop) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${prop.githubUser}.png`}
+        style={{ borderRadius: '8px' }}
+      />
+    </Box>
+  )
+}
+
+export default function Home() {
+  const githubUser = 'milenayamamoto'
+  const pessoasFavoritas = [
+    'peas',
+    'omariosouto',
+    'marcobrunodev',
+    'juunegreiros'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className='profileArea' style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className='welcomeArea' style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className='title'>Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div
+          className='profileRelationsArea'
+          style={{ gridArea: 'profileRelationsArea' }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+              Pessoas da Comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map(itemAtual => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
